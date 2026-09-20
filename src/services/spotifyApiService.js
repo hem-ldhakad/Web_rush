bc6877df235deb1df6fe1fcde6ee282be962ff5a
hem-ldhakad <hemal.121855@gmail.com>
2026-09-20 15:07:12 +0530

    feat: export Song class for Spotipy-style OOP query and pagination

--- src/services/spotifyApiService.js
+++ src/services/spotifyApiService.js
@@ -98,6 +98,36 @@ export function* paginateSongs(records = [], query = '', batchSize = 100) {
 }
 
 /**
+ * Class representation of Spotipy-like Song API client for object-oriented querying and pagination.
+ */
+export class Song {
+  constructor(records = []) {
+    this.records = records;
+  }
+
+  /**
+   * Set or update dataset records
+   */
+  setRecords(records) {
+    this.records = records;
+  }
+
+  /**
+   * Query songs matching a string with limit
+   */
+  query_songs(query = '', limit = 20) {
+    return querySongs(this.records, query, limit);
+  }
+
+  /**
+   * Paginate songs matching a string in batches
+   */
+  paginate_songs(query = '', batchSize = 100) {
+    return paginateSongs(this.records, query, batchSize);
+  }
+}
+
+/**
  * Normalizes Spotify searchV2 items array into flat track objects for playback
  */
 export function extractTracksFromSearchV2(items = []) {
@@ -123,3 +153,4 @@ export function extractTracksFromSearchV2(items = []) {
     };
   });
 }
+
